18dddfc6c44e73f220fe2938227289a7148faa50
Elysiayn <wendy.lemus.wl@gmail.com>
2021-03-19 08:52:38 -0400

    bug found w/ server start up

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -108,7 +108,7 @@ const thoughtController = {
     deleteReaction({ params }, res) {
         Thought.findOneAndUpdate(
             { _id: params.thoughtId },
-            { $pull: { reaction: { reactionId: params.reactionId } } },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
             {new: true }
         )
         .then(dbUserData => {
