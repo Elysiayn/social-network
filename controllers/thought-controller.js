b2cfe111552d55da5a13fcc46e2c1aba8b599164
Elysiayn <wendy.lemus.wl@gmail.com>
2021-03-19 08:18:17 -0400

    updated updateUser to handle if no id found

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -11,8 +11,53 @@ const thoughtController = {
             res.sendSatus(400).json(err);
         });
     },
+
     // get thought by id
     getThoughtById({ params }, res) {
         Thought.findOne({ _id: params.id })
-    }
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    // create thought
+    createThought({ params }, res) {
+        Thought.create({
+            thoughtText: body.thoughtText,
+            username: body.username,
+            userId: params.userId
+        })
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: params.userId },
+                { $push: { thoughts: _id } },
+                { runValidators: true, new: true }
+            );
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    // update thought
+
+
+    // delete thought
+
+
+    // create reaction
+
+
+    // delete reaction
 }
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -32,11 +32,14 @@ const userController = {
             select: '-__v'
         })
         .select(-__v)
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.sendStatus(400).json(err);
-        });
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
     },
 
     // create user
@@ -48,7 +51,8 @@ const userController = {
 
     // update user by id
     updateUser({ params, body }, res) {
-        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        User.findOneAndUpdate(
+            { _id: params.id }, body, { new: true, runValidators: true })
         .then(dbUserData => {
             if (!dbUserData) {
                 res.status(404).json({ message: 'No user found with this id!' });
